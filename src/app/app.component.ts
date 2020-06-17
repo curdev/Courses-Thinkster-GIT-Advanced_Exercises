e02fd560262845aeef71c7d04c106c1dd9faef54
Curtis McLaws <cortescurtis@gmail.com>
2020-06-17 01:12:05 -0600

    Added bootstrap and started the flopProject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'AdvancedGIT-Example';
+export class AppComponent  {
+  public name: string = "Curtis McLaws";
+  public favFlop: {title: string
+    , yearReleased: number
+    , rating: string
+    , imdbLink: string
+    , soundtrack: {
+      youTubeMusicLink: string
+    }
+  } = { 
+      title: "Solo"
+    , yearReleased: 2018
+    , rating: "PG-13"
+    , imdbLink: "https://www.imdb.com/title/tt3778644/?ref_=nv_sr_srsg_0"
+    , soundtrack: {
+      youTubeMusicLink: "https://music.youtube.com/playlist?list=OLAK5uy_m-gVRU1K3SzRoZvlehjy-HfK76AhA0jTc"
+    }
+  }
 }
