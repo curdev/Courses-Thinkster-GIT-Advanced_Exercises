202bf792fa8f6bd898b42eca524edbb2508c85ec
Curtis McLaws <cortescurtis@gmail.com>
2020-06-17 02:52:23 -0600

    Added images and the movies component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,22 @@
 import { Component } from '@angular/core';
 
+export interface Movie {title: string
+  , yearReleased: number
+  , rating: string
+  , imdbLink: string
+  , soundtrack: {
+    youTubeMusicLink: string
+  }
+};
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  public name: string = "Curtis McLaws";
-  public favFlop: {title: string
-    , yearReleased: number
-    , rating: string
-    , imdbLink: string
-    , soundtrack: {
-      youTubeMusicLink: string
-    }
-  } = { 
+  
+  public favFlop: Movie =  { 
       title: "Solo"
     , yearReleased: 2018
     , rating: "PG-13"
@@ -23,4 +25,10 @@ export class AppComponent  {
       youTubeMusicLink: "https://music.youtube.com/playlist?list=OLAK5uy_m-gVRU1K3SzRoZvlehjy-HfK76AhA0jTc"
     }
   }
+
+  public onCheckout = (event: any) => console.log('check out', event);
+  
+  public textClick = (event: any) => console.log('data clicked:', event.target.innerText);
+
+  public titleHover = (message: string) => console.log(message + ' the title:', this.favFlop.title);
 }
